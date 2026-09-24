605d79ecd08a15145f4c4ed056d461d85857faa6
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-24 08:11:22 +0300

    Harden mobile input lifecycle for V3.16.7

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'orbit-io-v3-16-6';
+const CACHE = 'orbit-io-v3-16-7';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
