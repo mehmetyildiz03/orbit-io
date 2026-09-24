f551543580d6e616671030de81983b9abfa1e0d1
Mehmet Yıldız <115743794+mehmetyildiz03@users.noreply.github.com>
2026-09-24 15:08:36 +0300

    Add smooth mobile camera hysteresis in V3.16.9

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'orbit-io-v3-16-8';
+const CACHE = 'orbit-io-v3-16-9';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
